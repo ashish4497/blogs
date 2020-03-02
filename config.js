4233bb22d4bbec27f1ac0a811c2cd003bb465757
ashish497 <ak8393267@gmail.com>
2020-03-02 14:49:07 +0530

    changes in links

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // the name of your website
-  title: 'Ashish Kumar',
+  title: 'Ashish Sharma',
   // the description of the website (eg. what shows on Google)
   description: "Ashish Kumar's blog",
   // a short bio shown at the bottom of your blog posts
@@ -8,17 +8,17 @@ module.exports = {
   shortBio: '',
   // a longer bio showing on the landing page of the blog
   bio: ``,
-  author: 'Ashish Kumar',
+  author: 'Ashish Sharma',
   githubUrl: 'https://github.com/',
   // replace this by the url where your website will be published
   siteUrl: 'http://localhost:8000',
   social: {
     // leave the social media you do not want to appear as empty strings
-    twitter: '',
+    twitter: 'https://twitter.com/Ashish4497',
     medium: '@ak8393267',
-    facebook: '',
-    github: '',
-    linkedin: '',
-    instagram: '',
+    facebook: 'https://www.facebook.com/ashish4497',
+    github: 'https://github.com/ashish4497',
+    linkedin: 'https://www.linkedin.com/in/ashish-sharma-693240165/',
+    instagram: 'https://www.instagram.com/ashishs23io/',
   },
 }
